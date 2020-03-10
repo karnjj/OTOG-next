cf2c747c7ff475dc0bf7f5f4f0d2aca7a2c84ffb
Karnjj <karnjj@hotmail.com>
2020-03-10 22:28:20 +0700

    show pdf

--- client/pages/problem/[name].js
+++ client/pages/problem/[name].js
@@ -1,12 +1,23 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import App from '../../components/App';
+    import { Navbar, Nav, Button  } from 'react-bootstrap';
+import { withAuthSync } from '../../utils/auth';
+import Submit from '../../components/Submit';
 const openPDF = props => {
     const router = useRouter()
     const { name } = router.query
-    const url = 'http://localhost:8000/api/docs/'+name
+    const url = 'http://localhost:8000/api/docs/' + name
     return (
         <App>
+            <Navbar sticky="top" expand="sm" bg="dark" variant="dark">
+                <Navbar.Brand>{name}</Navbar.Brand>
+                <Nav className="mr-auto" ></Nav>
+                <Nav className="mr-auto" ><code><h4>PPPPPPPPPPP</h4></code></Nav>
+                <Nav className="justify-content-end">
+                    <Submit prob={{name}} />
+                </Nav>
+            </Navbar>
             <iframe src={url} type="application/pdf" >   </iframe>
             <style jsx>{`
                 iframe {
@@ -20,4 +31,4 @@ const openPDF = props => {
         </App>
     )
 }
-export default openPDF
+export default withAuthSync(openPDF)
