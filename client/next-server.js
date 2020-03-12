2d2d93d962d5a01cbfa1cf1127207390f7c8af15
Karnjj <karnjj@hotmail.com>
2020-03-12 22:06:48 +0700

    update port

--- client/next-server.js
+++ client/next-server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 3002
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -13,9 +14,9 @@ app
       return handle(req, res);
     });
 
-    server.listen(3000, err => {
+    server.listen(port, err => {
       if (err) throw err;
-      console.log("> Next.js ready on http://localhost:3000");
+      console.log(`> Next.js ready on http://localhost:${port}`);
     });
   })
   .catch(ex => {
