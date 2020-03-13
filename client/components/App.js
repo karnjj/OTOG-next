02556ff51acd718204cd0ea52b99dd83866c905d
Karnjj <karnjj@hotmail.com>
2020-03-13 10:50:13 +0700

    ✨add manifest

--- client/components/App.js
+++ client/components/App.js
@@ -6,6 +6,10 @@ export default function App({ children }) {
     return(
         <main>
             <Head>
+                <title>openOTOG</title>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/logoIOS.png"></link>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 {/*<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>*/}
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             </Head>
