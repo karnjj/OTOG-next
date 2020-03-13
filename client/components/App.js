1895e36d040fa38ce337fb333ccbf1d0a60d8de8
Karnjj <karnjj@hotmail.com>
2020-03-13 10:55:44 +0700

    set theme color

--- client/components/App.js
+++ client/components/App.js
@@ -10,6 +10,7 @@ export default function App({ children }) {
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="apple-touch-icon" href="/logoIOS.png"></link>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="theme-color" content="#ff851b"></meta>
                 {/*<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>*/}
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             </Head>
