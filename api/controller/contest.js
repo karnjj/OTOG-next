e5706e79685dcb11fd764eaa55edc4eb5a1d24ae
Karnjj <karnjj@hotmail.com>
2020-03-28 16:31:58 +0700

    fix scoreboard bug

--- api/controller/contest.js
+++ api/controller/contest.js
@@ -29,21 +29,23 @@ function getContestWithId(req,res) {
 
 function getContestHistoryWithId(req,res) {
     const idContest = req.params.idContest
-	let sql = `select U.idUser,sname,R1.prob_id,score from 
-		(select user_id,prob_id,max(time) as lastest from Result where contestmode = ? group by user_id,prob_id) as R1 
-		inner join Result as R2 on R1.user_id = R2.user_id and R1.prob_id = R2.prob_id and R1.lastest = R2.time 
+	let sql = `select U.idUser,sname,R1.prob_id,score,timeuse from 
+		(select user_id,prob_id,max(idResult) as lastest from Result where contestmode = ? group by user_id,prob_id) as R1 
+		inner join Result as R2 on R1.user_id = R2.user_id and R1.prob_id = R2.prob_id and R1.lastest = R2.idResult 
 		inner join User as U on U.idUser = R1.user_id where U.state = 1 order by U.idUser,R1.prob_id`
 	db.query(sql, [idContest], (err, result) => {
 		var new_result = []
 		var info = {}
 		result.map((data,index) => {
 			if(info.sum === undefined) info.sum = 0
+			if(info.sumTime === undefined) info.sumTime = 0
 			if(info.scores === undefined) info.scores = {}
 			if(info.scores[data.prob_id] === undefined) info.scores[data.prob_id] = {}
 			info.idUser = data.idUser
 			info.sname = data.sname
-			info.sum = Number(info.sum) + Number(data.score)
-			info.scores[data.prob_id].score = data.score
+			info.sum +=  Number(Math.floor(data.score))
+			info.sumTime +=  Number(data.timeuse)
+			info.scores[data.prob_id].score =  Number(Math.floor(data.score))
 			var next = result[Number(index)+1] || {idUser:-1}
 			if(data.idUser != next.idUser) {
 				new_result.push(info)
@@ -51,6 +53,11 @@ function getContestHistoryWithId(req,res) {
 			}
 		})
 		new_result.sort((a,b) => b.sum - a.sum)
+		var Rank = 1
+		for(var idx in new_result) {
+			 if (idx != 0) if(new_result[idx].sum != new_result[idx-1].sum) Rank = Number(idx)+1
+			new_result[idx].rank = Rank 
+		}
 		res.json(new_result)
 	});
 }
