d71844c5373c9dee31197fb85afe26fc79206fd7
Karnjj <karnjj@hotmail.com>
2020-04-01 21:57:41 +0700

    analyze

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css')
 const withOffline = require('next-offline')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
 const prod = process.env.NODE_ENV === 'production'
 const nextConfig = {
     env: {
@@ -9,5 +12,6 @@ const nextConfig = {
 }
 module.exports = withPlugins([
     [withCSS],
-    [withOffline]
+    [withOffline],
+    [withBundleAnalyzer]
 ], nextConfig)
