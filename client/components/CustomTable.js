604967c161e4f63a2b71e57bd2baa2105cee4e25
Anos Posidos <anonjin44@gmail.com>
2020-06-05 15:28:27 +0700

    💅 change table display animation

--- client/components/CustomTable.js
+++ client/components/CustomTable.js
@@ -25,11 +25,9 @@ export const UserTd = (props) => (
 const fadein = keyframes`
 	0% {
 		opacity: 0;
-		transform : translateY(-5px);
 	}
 	100% {
 		opacity: 1;
-		transform: translateY(0px);
 	}
 `
 const CenterTable = styled(Table)`
